c051555a6f47cb43dae9a89049ccd0b5d525b387
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-05-04 23:28:21 +0500

    authentication added with login

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,45 @@
 const User = require("../models/users")
+const {v4:uuidv4} = require("uuid")
+const {setUser , getUser} = require("../service/auth")
+async function handleUserSignUp(req, res) {
+    const { name, email, password } = req.body;
 
-async function handleUserSignUp(req,res){
-    const {name, email, password}= req.body
-    await User.create({
-        name,
-        email,
-        password
-    })
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.render("signup", {
+                error: "Email is already registered. Try logging in instead."
+            });
+        }
 
-    return res.redirect("/")
+        await User.create({
+            name,
+            email,
+            password
+        });
 
+        return res.redirect("/login");
+    } catch (err) {
+        console.error("Signup Error:", err);
+        return res.status(500).send("Internal Server Error");
+    }
 }
+
 
 
 async function handleUserLogin(req,res){
     const { email, password}= req.body
-    console.log(user)
+ 
     const user = await User.findOne({email,password})
     if(!user){
         return res.render("login",{
             error:"Invalid Username or password"
         })
     }
+
+    const sessionId = uuidv4()
+    setUser(sessionId , user)
+    res.cookie("uid", sessionId)
     return res.redirect("/")
 
 }
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express")
 const connectToMongoDb = require("./connect")
 const urlRoute = require("./routes/url")
 const path = require("path")
+const cookieParser = require("cookie-parser")
 const URL = require("./models/url")
 const staticRouter = require("./routes/StaticRouter")
 const userRoute = require("./routes/user")
+const {restrictToLoggedinUserOnly} = require("./middlewares/auth")
 const app = express()
 const PORT = 8001
 
@@ -16,8 +18,9 @@ connectToMongoDb("mongodb://127.0.0.1:27017/shortUrl").then(()=>console.log("mon
 
     app.use(express.json())
     app.use(express.urlencoded({extended:false}))
+    app.use(cookieParser())
 
-    app.use("/url", urlRoute)
+    app.use("/url",restrictToLoggedinUserOnly, urlRoute)
     app.use("/user", userRoute)
     app.use("/", staticRouter)
     
