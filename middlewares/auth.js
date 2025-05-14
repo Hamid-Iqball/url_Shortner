ce25e4a83a976f7fec1492818b3554e03d8bbee1
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-05-14 23:54:02 +0500

    jwt authentications

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,9 @@ const { getUser } = require("../service/auth")
 
 async function restrictToLoggedinUserOnly(req,res,next){
     const userUid = req.cookies?.uid
+    console.log(req)
     if(!userUid) return res.redirect("/login")
-  
-    const user = getUser(userUid)
+        const user = getUser(userUid)
     if(!user) return res.redirect("/login")
     req.user = user
     next()
